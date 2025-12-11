5cbfa32ada7060edbe307619033a932a521b4688
627846 <130502753+627846@users.noreply.github.com>
2025-12-11 12:40:31 -0800

    Create get_hike_info.py
    A function to get the locations, ratings, and names of all hikes within 25 km of central Davis, WIP

---
+++ get_hike_locations.py
@@ -0,0 +1,66 @@
+import requests
+import pandas as pd
+import time
+import json
+import lxml.html as lx
+
+def read_key(keyfile):
+    with open(keyfile) as f:
+        return f.readline().strip("\n")
+key = read_key("API_Key_google.txt")
+
+url = "https://places.googleapis.com/v1/places:searchText"
+
+def get_hike_locations():
+    result = requests.post(
+        url=url,
+        headers={
+            "Content-Type": "application/json",
+            "X-Goog-Api-Key": key,
+            "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,nextPageToken"
+        },
+        json={                                    
+            "textQuery": "hiking_area",
+            "locationBias": {
+                "circle": {
+                    "center": {
+                        "latitude": 38.551683,
+                        "longitude": -121.749776
+                    },
+                    "radius": 25000
+                }
+            },
+        }
+    )
+    resultJson = result.json()
+    nextPage = resultJson.get("nextPageToken")
+    lst = [resultJson.get("places")[i].get("displayName") for i in range(len(resultJson.get("places")))]
+    if nextPage:
+        result2 = requests.post(
+            url=url,
+            headers={
+                "Content-Type": "application/json",
+                "pageToken": nextPage,
+                "X-Goog-Api-Key": key,
+                "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,nextPageToken"
+            },
+            json={                                    
+                "textQuery": "hiking_area",
+                "locationBias": {
+                    "circle": {
+                        "center": {
+                            "latitude": 38.551683,
+                            "longitude": -121.749776
+                        },
+                        "radius": 25000
+                    }
+                },
+            }
+        )
+        lst.append(str(len(lst)) + " total results")
+        resultJson2 = result2.json()
+        return print(pd.DataFrame([resultJson2.get("places")[i].get("displayName") for i in range(len(resultJson2.get("places")))])), "one"
+    else:
+        return print(pd.DataFrame([resultJson.get("places")[i].get("displayName") for i in range(len(resultJson.get("places")))])), "two"
+
+get_hike_locations()
