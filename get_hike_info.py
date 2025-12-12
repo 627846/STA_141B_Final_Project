3dc53994d5223b2d3e29e57fce73d6c7288e5690
627846 <130502753+627846@users.noreply.github.com>
2025-12-11 20:19:29 -0800

    Update get_hike_info.py

--- get_hike_info.py
+++ get_hike_info.py
@@ -3,6 +3,7 @@ import pandas as pd
 import time
 import json
 import lxml.html as lx
+from geopy import distance
 
 def read_key(keyfile):
     with open(keyfile) as f:
@@ -12,39 +13,18 @@ key = read_key("API_Key_google.txt")
 url = "https://places.googleapis.com/v1/places:searchText"
 
 def get_hike_info():
-    result = requests.post(
-        url=url,
-        headers={
-            "Content-Type": "application/json",
-            "X-Goog-Api-Key": key,
-            "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,nextPageToken"
-        },
-        json={                                    
-            "textQuery": "hiking_area",
-            "locationBias": {
-                "circle": {
-                    "center": {
-                        "latitude": 38.551683,
-                        "longitude": -121.749776
-                    },
-                    "radius": 25000
-                }
-            },
-        }
-    )
-    resultJson = result.json()
-    nextPage = resultJson.get("nextPageToken")
-    lst = [resultJson.get("places")[i].get("displayName") for i in range(len(resultJson.get("places")))]
-    if nextPage:
-        result2 = requests.post(
+    lst = []
+    nextPage = None
+    while True:
+        result = requests.post(
             url=url,
             headers={
                 "Content-Type": "application/json",
-                "pageToken": nextPage,
                 "X-Goog-Api-Key": key,
-                "X-Goog-FieldMask": "places.displayName,places.formattedAddress,places.rating,nextPageToken"
+                "X-Goog-FieldMask": "places.displayName,places.location,places.rating,nextPageToken"
             },
-            json={                                    
+            json={
+                "pageToken": nextPage,
                 "textQuery": "hiking_area",
                 "locationBias": {
                     "circle": {
@@ -52,15 +32,40 @@ def get_hike_info():
                             "latitude": 38.551683,
                             "longitude": -121.749776
                         },
-                        "radius": 25000
+                        "radius": 35000
                     }
                 },
             }
         )
-        lst.append(str(len(lst)) + " total results")
-        resultJson2 = result2.json()
-        return print(pd.DataFrame([resultJson2.get("places")[i].get("displayName") for i in range(len(resultJson2.get("places")))])), "one"
-    else:
-        return print(pd.DataFrame([resultJson.get("places")[i].get("displayName") for i in range(len(resultJson.get("places")))])), "two"
+        resultJson = result.json()
+        [lst.append(resultJson.get("places")[i]) for i in range(len(resultJson.get("places")))]
+        
+        if resultJson.get("nextPageToken"):
+            nextPage = resultJson.get("nextPageToken")
+        else: 
+            return lst
+        time.sleep(3)
+df = get_hike_info()
 
+def get_best_hike(df):
+    rating = [df[i].get("rating") for i in range(len(df))]
+    maxRating = max(rating)
+    [df[i].get("rating") for i in range(len(df))]
+    best = [df[i].get("displayName").get("text") for i in range(len(df)) if maxRating == df[i].get("rating")]
+    return get_closest_hike(best)
+
+def get_closest_hike(location):
+    centralDavis = (38.551683, -121.749776)
+    latitude = [df[i].get("location").get("latitude") 
+                    for i in range(len(df)) for k in range(len(location)) if df[i].get("displayName").get("text") == location[k]]
+    longitude = [df[i].get("location").get("longitude") 
+                    for i in range(len(df)) for k in range(len(location)) if df[i].get("displayName").get("text") == location[k]]
+    loc = [[latitude[i], longitude[i]] for i in range(len(latitude))]
+    dist = [distance.distance(loc[i], centralDavis).km for i in range(len(loc))]
+    closest = dist.index(min(dist))
+    temp = loc[closest]
+    result = [df[i].get("displayName").get("text")
+                    for i in range(len(df)) if df[i].get("location").get("latitude") == temp[0] and df[i].get("location").get("longitude") == temp[1]]
+    return result
 get_hike_info()
+get_best_hike(df)
